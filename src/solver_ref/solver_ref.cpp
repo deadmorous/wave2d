9f2504b6f28129c88998cae919cbe38f6acca218
Stepan Orlov <majorsteve@mail.ru>
2019-10-23 12:25:51 +0300

    Implemented reference solver output.

--- src/solver_ref/solver_ref.cpp
+++ src/solver_ref/solver_ref.cpp
@@ -14,36 +14,21 @@ public:
             const DataFrame& fcur,
             DataFrame& fnext)
     {
-            for(unsigned int i; i<fprev.width();i++)
+        auto dst = fnext.data();
+        for(auto j=0u; j<fprev.height();j++)
+        {
+            for(auto i=0u; i<fprev.width();i++)
             {
-                for(unsigned int j; j<fprev.height();j++)
-                {
-                    unsigned int i_plus;
-                    unsigned int i_minus;
-                    unsigned int j_plus;
-                    unsigned int j_minus;
-                    if (i==fprev.width()-1)
-                        i_plus=0;
-                    else
-                        i_plus=i+1;
-                    if (i==0)
-                        i_minus=fprev.width()-1;
-                    else
-                        i_minus=i-1;
-                    if (j==fprev.width()-1)
-                        j_plus=0;
-                    else
-                        j_plus=j+1;
-                    if (j==0)
-                        j_minus=fprev.width()-1;
-                    else
-                       j_minus=j-1;
-                    real_type gamma = modelParameters.getWaveSpeed()*
-                                   solverParameters.getTimeStepLength()/solverParameters.getSpatialStepLength();
-                    real_type lapl = fcur.at(i_plus,j)+fcur.at(i,j_plus)+fcur.at(i_minus,j)+fcur.at(i,j_minus)-4*fcur.at(i,j);
-                    real_type u=2*fcur.at(i,j)-fprev.at(i,j)+gamma*gamma*lapl;
-
-                }
+                auto i_plus = i==fprev.width()-1? 0: i+1;
+                auto i_minus = i==0? fprev.width()-1: i-1;
+                auto j_plus = j==fprev.width()-1? 0: j+1;
+                auto j_minus = j==0? fprev.width()-1: j-1;
+                real_type gamma = modelParameters.getWaveSpeed()*
+                               solverParameters.getTimeStepLength()/solverParameters.getSpatialStepLength();
+                real_type lapl = fcur.at(i_plus,j)+fcur.at(i,j_plus)+fcur.at(i_minus,j)+fcur.at(i,j_minus)-4*fcur.at(i,j);
+                real_type u=2*fcur.at(i,j)-fprev.at(i,j)+gamma*gamma*lapl;
+                *dst++ = u;
             }
+        }
     }
 };
